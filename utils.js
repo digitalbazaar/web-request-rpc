82e030d19c51b60a6fafb608fec23e44cb909b78
Dave Longley <dlongley@digitalbazaar.com>
2017-08-02 11:26:27 -0400

    Add `originMatches` utility function.

--- utils.js
+++ utils.js
@@ -79,6 +79,14 @@ export function parseUrl(url, base) {
   };
 }
 
+export function originMatches(url, origin) {
+  const parsed = parseUrl(url, origin);
+  if(parsed.origin !== origin) {
+    throw new Error(
+      `Origin mismatch. Url "${url}" does not have an origin of "${origin}".`);
+  }
+}
+
 // https://gist.github.com/LeverOne/1308368
 export function uuidv4(a,b) {
   for(b=a='';a++<36;b+=a*51&52?(a^15?8^Math.random()*(a^20?16:4):4).toString(16):'-');return b;
