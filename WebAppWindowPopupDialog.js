1e4807d97a8d4a91430ade777f7aade572530327
Ganesh Annan <gannan@digitalbazaar.com>
2022-06-07 18:08:48 -0400

    Re-enable event listener management.

--- WebAppWindowPopupDialog.js
+++ WebAppWindowPopupDialog.js
@@ -39,12 +39,50 @@ export class WebAppWindowPopupDialog extends WebAppWindowDialog {
       `width=${width},height=${height},left=${left},top=${top}`;
     this.handle = window.open(url, name, features);
 
-    // this.handle.addEventListener('load', () => {
-    //   this.handle.addEventListener('unload', () => {
-    //     this.destroyed = true;
-    //   }, {once: true});
-    // }, {once: true});
+    this.setListeners();
+  }
 
-    // window.addEventListener('unload', () => this.destroy(), {once: true});
+  setLocation(location) {
+    this.removeListeners();
+    this.handle.location.href = location;
+    this.setListeners();
+  }
+
+  setListeners() {
+    // create the on load handler that will set this.destroyed to true when
+    // an unload event comes in
+    if(!this._setDestroyedToTrueOnLoadHandler) {
+      // store handler function
+      this._setDestroyedToTrueOnLoadHandler = () => {
+        // create the on unload handler that will set this.destroyed to true, if
+        // not already created
+        if(!this._setDestroyedToTrue) {
+          this._setDestroyedToTrue = () => this.destroyed = true;
+        }
+        // add the unload handler
+        this.handle.addEventListener('unload', this._setDestroyedToTrue,
+          {once: true});
+      }
+    }
+
+    // create the on unload handler that will call this.destroy(), if no
+    // already created
+    if(!this._destroyUnloadHandler) {
+      this._destroyUnloadHandler = () => this.destroy();
+    }
+    // add the load handler
+    this.handle.addEventListener('load', this._setDestroyedToTrueOnLoadHandler,
+      {once: true});
+    // add the unload handler
+    window.addEventListener('unload', this._destroyUnloadHandler, {once: true});
+  }
+
+  removeListeners() {
+    this.handle.removeEventListener(
+      'load', this._setDestroyedToTrueOnLoadHandler, {once: true});
+    this.handle.removeEventListener('unload', this._setDestroyedToTrue,
+      {once: true});
+    window.removeEventListener('unload', this._destroyUnloadHandler,
+      {once: true});
   }
 }
